8b69b4aafaa29a7815dbf6830945b536b930ec5f
N Sumant <sumantforreal@gmail.com>
2026-01-13 04:51:29 +0000

    datatyoe

---
+++ 01.basic/0.2_basic.js
@@ -0,0 +1,14 @@
+"use strict ";// use all js code as newer version
+//data types
+/*
+number
+bigint
+undifined => it self a datatype
+null => object,standalone value
+string
+boolean
+
+*/
+let s;
+let b=null;
+console.table([typeof(s),typeof(b)])
