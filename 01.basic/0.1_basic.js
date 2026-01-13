a3aff9d8ddac189dff657828d58402be8aaa1699
N Sumant <sumantforreal@gmail.com>
2026-01-13 04:44:45 +0000

    datatype

---
+++ 01.basic/0.1_basic.js
@@ -0,0 +1,9 @@
+let ac=1
+names="sumant"
+var ai="goat"
+au=2
+let gu
+// names=sumant2 not allowd
+// var is not used because it doesnt have block scope and function
+//let can be change
+console.table([ac,names,au,ai,gu]) 
